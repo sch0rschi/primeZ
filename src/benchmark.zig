e9734059c9641352ebe18a896698aecac01ee519
sch0rschi <getschii@gmail.com>
2026-06-23 16:13:30 +0200

    benchmark several nth prime runtimes add rust runtimes as comment

--- src/benchmark.zig
+++ src/benchmark.zig
@@ -2,20 +2,59 @@ const std = @import("std");
 
 const StreamingSieve = @import("streamingSieve.zig").StreamingSieve;
 
-const LIMIT = 22_801_763_489;
-const NTH = 1_000_000_000;
-const RUNS = 1;
 
+// Rust primal
+// 100:         541             5.25 µs
+// 1000:        7919            9.17 µs
+// 10000:       104729          12.2 µs
+// 100000:      1299709         93.3 µs
+// 1000000:     15485863        838  µs
+// 10000000:    179424673       13.6 ms
+// 100000000:   2038074743      228  ms
+// 1000000000:  22801763489     3.65 s
+// 10000000000: 252097800623    61.1 s
 pub fn main(init: std.process.Init) !void {
     const allocator = init.gpa;
     const io = init.io;
 
-    const t0 = std.Io.Clock.now(.awake, io);
-    for (0..RUNS) |_| {
-        _ = try StreamingSieve.nthPrime(allocator, NTH);
-    }
-    const t1 = std.Io.Clock.now(.awake, io);
+    var limit: usize = 100;
+    while (limit <= 10_000_000_000) {
+        const t0 = std.Io.Clock.now(.awake, io);
+        const nthPrime = try StreamingSieve.nthPrime(allocator, limit-1);
+        const t1 = std.Io.Clock.now(.awake, io);
 
-    std.debug.print("Looking up {}-th Prime, average runtime: {}ms\n", .{ NTH, @as(f64, @floatFromInt(t0.durationTo(t1).toMilliseconds())) / RUNS });
+        const duration = try formatDuration(allocator, t0.durationTo(t1).toNanoseconds());
+        defer allocator.free(duration);
+
+        std.debug.print("{}: {} {s}\n", .{ limit,  nthPrime, duration });
+        limit *= 10;
+    }
 }
 
+pub fn formatDuration(allocator: std.mem.Allocator, ns: i96) ![]u8 {
+    const us = 1000;
+    const ms = us * 1000;
+    const s  = ms * 1000;
+
+    if (ns < us) {
+        return std.fmt.allocPrint(allocator, "{d}ns", .{ns});
+    } else if (ns < ms) {
+        return std.fmt.allocPrint(
+            allocator,
+            "{d:.2}µs",
+            .{@as(f64, @floatFromInt(ns)) / us},
+        );
+    } else if (ns < s) {
+        return std.fmt.allocPrint(
+            allocator,
+            "{d:.2}ms",
+            .{@as(f64, @floatFromInt(ns)) / ms},
+        );
+    } else {
+        return std.fmt.allocPrint(
+            allocator,
+            "{d:.2}s",
+            .{@as(f64, @floatFromInt(ns)) / s},
+        );
+    }
+}
