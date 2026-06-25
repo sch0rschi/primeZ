1740bb4410ab36750133d3d205be012efb2dd0c0
sch0rschi <getschii@gmail.com>
2026-06-26 01:59:50 +0200

    adapt root.zig

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,3 @@
-pub const QuerySieve = @import("querySieve.zig");
 pub const StreamingSieve = @import("streamingSieve.zig");
 pub const PrimeCheck = @import("primeCheck.zig");
 pub const Estimates = @import("estimates.zig");
