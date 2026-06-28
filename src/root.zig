3688e062370235551083cefba8e45c8e7922bdfc
sch0rschi <getschii@gmail.com>
2026-06-28 21:30:16 +0200

    expose PrimeStore Type in root

--- src/root.zig
+++ src/root.zig
@@ -1,4 +1,4 @@
 pub const Primes = @import("primes.zig");
 pub const PrimeCheck = @import("primeCheck.zig");
 pub const PrimeEstimates = @import("estimates.zig");
-pub const PrimeStore = @import("primeStore.zig");
+pub const PrimeStore = @import("primeStore.zig").PrimeStore;
